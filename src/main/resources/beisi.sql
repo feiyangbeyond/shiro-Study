769a120158911635bc87b6b2d68e41418d325937
赵鹏程 <Adrianzpc@163.com>
2019-02-09 11:46:24 +0800

    修改数据库文件

--- src/main/resources/beisi.sql
+++ src/main/resources/beisi.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50716
 File Encoding         : 65001
 
-Date: 2019-01-28 18:09:16
+Date: 2019-02-09 11:45:52
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -34,10 +34,10 @@ CREATE TABLE `t_permission` (
 -- ----------------------------
 INSERT INTO `t_permission` VALUES ('268793129a5c4ef9861c4b79e9d342f7', 'logout', '/logout.html', '2', '2019-01-28 12:24:45', null);
 INSERT INTO `t_permission` VALUES ('45b4d55b15c84dfc91dd627189d4ffbd', 'authc', '/admin/**', '6', '2019-01-28 12:26:02', null);
-INSERT INTO `t_permission` VALUES ('99ec8ff1470d41e3bae7755240a90f41', 'p:userlist', '/admin/userlist.html', '3', '2019-01-28 12:25:09', null);
+INSERT INTO `t_permission` VALUES ('99ec8ff1470d41e3bae7755240a90f41', 'p:userlist', '/admin/userManager.html', '3', '2019-01-28 12:25:09', null);
 INSERT INTO `t_permission` VALUES ('b5e833fa35d94e9d884fe53f8461a9df', 'anon', '/**', '7', '2019-01-28 12:26:21', null);
 INSERT INTO `t_permission` VALUES ('d5fde8914cc14282b9b0d9e2178c7433', 'user', '/admin/main.html', '5', '2019-01-28 12:25:47', null);
-INSERT INTO `t_permission` VALUES ('d8a6581267054211b452ae57df5f5e29', 'p:adduser', '/admin/adduser.html', '4', '2019-01-28 12:25:28', null);
+INSERT INTO `t_permission` VALUES ('d8a6581267054211b452ae57df5f5e29', 'p:adduser', '/admin/addUser.html', '4', '2019-01-28 12:25:28', null);
 INSERT INTO `t_permission` VALUES ('e5bc14bc25d94365b9569ab4f1a3bdcd', 'anon', '/login.html', '1', '2019-01-28 12:24:27', null);
 
 -- ----------------------------
@@ -56,7 +56,8 @@ CREATE TABLE `t_role` (
 -- ----------------------------
 -- Records of t_role
 -- ----------------------------
-INSERT INTO `t_role` VALUES ('10f2863479874c1490f4c00b46184765', '管理员', 'admin', '2019-01-28 12:23:05', null);
+INSERT INTO `t_role` VALUES ('10f2863479874c1490f4c00b46184765', '超级管理员', 'admin', '2019-01-28 12:23:05', null);
+INSERT INTO `t_role` VALUES ('10f2863479874c1490f4c99b46184765', '管理员', 'root', '2019-02-08 11:39:48', null);
 
 -- ----------------------------
 -- Table structure for t_role_permission
@@ -96,6 +97,7 @@ CREATE TABLE `t_user` (
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
+INSERT INTO `t_user` VALUES ('833de7c8bf0f4328b75d709002705c49', 'test', '22f4fdae4b5448b69a07a25352757415', 'Adrianzpc@163.com', '13333333333', '1', '2019-02-09 11:16:37', null);
 INSERT INTO `t_user` VALUES ('9c06bcaaa1794c74ad395cd0ec5d6a25', 'admin', 'a7c4a89393dd48e3b755211351c49ffa', 'pop@163.com', '18888888888', '1', '2019-01-17 09:01:03', null);
 
 -- ----------------------------
@@ -114,4 +116,6 @@ CREATE TABLE `t_user_role` (
 -- ----------------------------
 -- Records of t_user_role
 -- ----------------------------
+INSERT INTO `t_user_role` VALUES ('7de8eda4ab904637a05cdf37e00e6871', '833de7c8bf0f4328b75d709002705c49', '10f2863479874c1490f4c99b46184765', '2019-02-09 11:16:37', null);
 INSERT INTO `t_user_role` VALUES ('8f3b0da967654103b073fe1424e19787', '9c06bcaaa1794c74ad395cd0ec5d6a25', '10f2863479874c1490f4c00b46184765', '2019-01-28 12:23:51', null);
+INSERT INTO `t_user_role` VALUES ('8f3b0da967654103b073fe1424e19799', '9c06bcaaa1794c74ad395cd0ec5d6a25', '10f2863479874c1490f4c99b46184765', '2019-02-08 11:40:35', null);
